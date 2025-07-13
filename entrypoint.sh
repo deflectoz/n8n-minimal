d881e0dee7471893aed08b38daa11fac5244b029
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2025-07-13 23:57:41 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo "[ENTRYPOINT] Fixing permission on volume..."
+echo "[ENTRYPOINT] Checking DB file..."
 
-# Coba dulu tanpa chown, biar kita lihat output
-ls -l /home/node/.n8n
+ls -l /home/node/.n8n || echo "Folder not accessible"
 
-# Jalankan n8n langsung sebagai root dulu (biar gak kehalang permission)
+if [ -f /home/node/.n8n/database.sqlite ]; then
+  echo "✅ DB File exists!"
+else
+  echo "❌ DB File NOT FOUND (akan dibuat oleh n8n jika bisa nulis)"
+fi
 exec n8n
